a84d8c5cdfff3c733414ab14689caf233283c260
Jiří Peška <naruto987@seznam.cz>
2019-05-14 23:43:49 +0200

    Added records with enemy and equipment stats.

---
+++ Potreby/EditorNepratel.php
@@ -0,0 +1,24 @@
+﻿<?php
+
+class EditorNepratel {
+
+	public $nepritel;
+	public $utok;
+	
+	public function pridejNepritele($nepritel) {
+		//vytvor soubor txt s nepritelem..
+	}
+	
+	public function upravNepritele() {
+		// nacte staty do promenych a pak bude moznost upravit jeden urcity udaj
+	}
+	
+	public function smazNepritele() {
+		
+	}
+	
+	public function vypisNepritele($kriterium) {
+		//podle jmena/sily/...
+	}
+
+}
